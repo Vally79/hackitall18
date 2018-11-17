59941f27925b2a72fe7c8054c2fff26aea91059d
Alex <alex.banu14@gmail.com>
2018-11-17 17:20:40 +0200

    Finished get route

--- app/Http/Controllers/RouteController.php
+++ app/Http/Controllers/RouteController.php
@@ -10,6 +10,12 @@ class RouteController extends Controller
 {
     private $chargingStations;
 
+    /**
+     * Return a list of coordinates for each charging station in a country.
+     *
+     * @param string $country
+     * @return array|bool
+     */
     public function getChargingStations($country = 'romania')
     {
         $client = new Client();
@@ -17,15 +23,13 @@ class RouteController extends Controller
         $response = $client->get($url);
 
         if ($response->getStatusCode() != 200) {
-            return JsonResponse::create([
-                'status' => 'error',
-                'message' => 'External API is not responding'
-            ], 500);
+            return false;
         }
 
         $body = (string)$response->getBody();
         $items = json_decode($body);
         $coordinates = [];
+
         foreach ($items as $item) {
             $coordinates[] = [
                 'lat' => $item->lat,
@@ -34,6 +38,46 @@ class RouteController extends Controller
         }
 
         return $coordinates;
+    }
+
+    /**
+     * Return the route distance in km, duration in hours and waypoints from start to end point. Example:
+     * $startPoint = ['lat' => 23, 'lon' => 40], $endPoint = ['lat' => 23', 'lon' => 41]
+     *
+     * @param $startPoint
+     * @param $endPoint
+     * @return array|bool
+     */
+    public function getRoute($startPoint, $endPoint)
+    {
+        $client = new Client();
+        $url = "https://api.openrouteservice.org/directions?api_key=" . env('ORS_KEY') .
+            "&coordinates=" . $startPoint['lon'] . "," . $startPoint['lat'] . "|" . $endPoint['lon'] . "," . $endPoint['lat'] .
+            "&profile=driving-car" .
+            "&units=km" .
+            "&geometry_format=geojson";
+        $response = $client->get($url);
+
+        if ($response->getStatusCode() != 200) {
+            return false;
+        }
+
+        $body = (string)$response->getBody();
+        $decoded = json_decode($body);
+        $details = [
+            'distance' => $decoded->routes[0]->summary->distance,
+            'duration' => $decoded->routes[0]->summary->duration / 360,
+        ];
+        $details['coordinates'] = [];
+
+        foreach ($decoded->routes[0]->geometry->coordinates as $coordinate) {
+            $details['coordinates'][] = [
+                'lat' => $coordinate[0],
+                'lon' => $coordinate[1]
+            ];
+        }
+
+        return $details;
     }
 
     public function main(Request $request)
