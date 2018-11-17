76d3c62304bb7f9301178b642a3ab8a0a7d645a1
Vali <calinescuvali97v@gmail.com>
2018-11-17 17:44:08 +0200

    da

--- app/Http/Controllers/RouteController.php
+++ app/Http/Controllers/RouteController.php
@@ -80,6 +80,92 @@ class RouteController extends Controller
         return $details;
     }
 
+    public function distance($lat1, $lon1, $lat2, $lon2) {
+
+        $theta = $lon1 - $lon2;
+        $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+        $dist = acos($dist);
+        $dist = rad2deg($dist);
+        $miles = $dist * 60 * 1.1515;
+        return ($miles * 1.609344);
+    }
+
+    public function computeRoute($start, $finish, $max_time)
+    {
+        $result = getRoute($start, $finish);
+        $time = $result['time'];
+
+        if ($time > $max_time)
+            return "No";
+
+        $raspuns = array();
+        $raspuns['time'] = 0;
+
+        $power = $result['distance'] * 0.175;
+        if ($power > 100) {
+            $min_sum = INF;
+            $chosen_station = -1;
+            foreach ($this->chargingStations as $station) {
+                $a = distance($start['lat'], $start['lon'], $station['lat'], $station['lon']);
+                $a += distance($finish['lat'], $finish['lon'], $station['lat'], $station['lon']);
+                if ($a < $min_sum) {
+                    $min_sum = $a;
+                    $chosen_station = $station;
+                }
+            }
+            $prima = computeRoute($start, $chosen_station, $max_time / 2 - 1);
+            $doua = computeRoute($chosen_station, $finish, $max_time / 2 - 1);
+            if ($prima == "No" || $doua == "No" || $prima['time'] + $doua['time'] > $max_time)
+                return "No";
+            else {
+                $raspuns = array();
+                $raspuns['time'] = $prima['time'] + $doua['time'] + 2;
+                $raspuns['drum'] = array_merge($prima['drum'], $doua['drum']);
+                return $raspuns;
+            }
+        } else {
+            $raspuns['drum'] = array();
+            array_push($raspuns['drum'], $start);
+            $atractii = getTouristAttractions($start, $finish, );
+            $directie = 0;
+            if (abs($start['lat'] - $finish - ['lat']) < abs($start['lon'] - $finish['lon']))
+                $directie = 1;
+            $marime = count($atractii);
+            $curent = $start;
+            $gasite = true;
+            while ($gasite) {
+                $dist = INF;
+                $gasite = false;
+                $catelea = 0;
+                $cate = count($atractii);
+                $candidat = $curent;
+                for ($i = 0; $i < $cate; $i++) {
+                    if ($directie == 0) {
+                        if ($curent['lat'] < $atractii[i]['lat'] && $atractii[i]['lat'] < $finish[i]['lat'] && distance($curent['lat'], $curent['lon'], $atractii[i]['lat'], $atractii[i]['lon']) < $dist) {
+                            $dist = distance($curent['lat'], $curent['lon'], $atractii[i]['lat'], $atractii[i]['lon']);
+                            $gasite = true;
+                            $candidat = $atractii[i];
+                        }
+                    } else {
+                        if ($curent['lon'] < $atractii[i]['lon'] && $atractii[i]['lon'] < $finish[i]['lon'] && distance($curent['lat'], $curent['lon'], $atractii[i]['lat'], $atractii[i]['lon']) < $dist) {
+                            $dist = distance($curent['lat'], $curent['lon'], $atractii[i]['lat'], $atractii[i]['lon']);
+                            $gasite = true;
+                            $candidat = $atractii[i];
+                        }
+                    }
+                }
+                $rezultat = getRoute($candidat, $finish);
+                if ($rezultat['time'] + $raspuns['time'] > $max_time) {
+                    $gasite = false;
+                    $raspuns['time'] += $rezultat['time'];
+                }
+                array_push($raspuns['drum'], $candidat);
+                $curent = $candidat;
+            }
+            return $raspuns;
+        }
+    }
+
     public function main(Request $request)
     {
         $this->chargingStations = $this->getChargingStations();
