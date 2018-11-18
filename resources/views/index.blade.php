31d163a6821810e0051c56688486620bdc199380
Andrei <andrei.zugravu97@gmail.com>
2018-11-18 07:30:07 +0200

    favicon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Fun Travelling</title>
+    <link rel="icon" type="image/ico" href="{{ URL::asset('favicon.ico') }}">
     <link rel="stylesheet" href="{{ URL::asset('css/components.css') }}">
     <link rel="stylesheet" href="{{ URL::asset('css/responsee.css') }}">
     <link rel="stylesheet" href="{{ URL::asset('css/icons.css') }}">
